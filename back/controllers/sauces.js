62beec3ddfa8a81a35ce30aab76c99d79b480cd3
thomas-708 <thomaslap@outlook.fr>
2022-04-29 13:18:21 +0200

    fix

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -28,23 +28,24 @@ exports.modifySauce = (req, res, next) => {
 
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id, userId: req.auth.userId}).then
-        (sauce => {
-           
-            if (!sauce) {
-                res.status(404).json({
-                  error: new Error('No such Sauce!')
-                });
-            }
-            
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
-        .catch(error => res.status(500).json({ error })); 
+    console.log(res.auth.userId);
+    // Sauce.findOne({ _id: req.params.id,  userId: req.auth.userId}).then
+    //     (sauce => {
+    //         console.log(userId);
+            // if (!sauce) {
+            //     res.status(404).json({
+            //       error: new Error('No such Sauce!')
+            //     });
+            // }
+            // 
+            // const filename = sauce.imageUrl.split('/images/')[1];
+            // fs.unlink(`images/${filename}`, () => {
+            //     Sauce.deleteOne({ _id: req.params.id })
+            //         .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
+            //         .catch(error => res.status(400).json({ error }));
+            // });
+        // })
+        // .catch(error => res.status(500).json({ error })); 
 };
 
 
