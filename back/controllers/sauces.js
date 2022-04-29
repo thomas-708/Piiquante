a2643600d02d0d9d61ffd2d0adbdff2bf9b4f787
thomas-708 <thomaslap@outlook.fr>
2022-04-29 14:49:51 +0200

    projet fix

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -21,31 +21,26 @@ exports.modifySauce = (req, res, next) => {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body};
-    Sauce.updateOne({ _id: req.params.id, userId: req.auth.userId}, { ...sauceObject, _id: req.params.id })
+    Sauces.updateOne({ _id: req.params.id, userId: req.body.userId}, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
       .catch(error => res.status(400).json({ error }));
 };
 
 
 exports.deleteSauce = (req, res, next) => {
-  
-    Sauces.findOne({ _id: req.params.id,  userId: req.auth.userId}).then
-        (sauce => {
-            console.log(userId);
-            if (!sauce) {
-                res.status(404).json({
-                  error: new Error('No such Sauce!')
-                });
-            }
-            
-            const filename = sauce.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
-        .catch(error => res.status(500).json({ error })); 
+ 
+    Sauces.findOne({ _id : req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split("/images/")[1]
+      fs.unlink(`images/${filename}`, () => {
+        Sauces.deleteOne({_id : req.params.id})
+    .then(res.status(200).json({ message: "Sauce supprimée" }))
+    .catch(error => res.status(400).json({ error }))
+    
+      })
+    })
+    .catch(error => res.status(500).json({ error }))
+      
 };
 
 
